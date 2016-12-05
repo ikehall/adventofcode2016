8f0ad17bd1d2cd031831b23ba4cf82ed4477b071
ikehall <ike.hall@gmail.com>
2016-12-04 22:57:16 -0600

    prettifying number3

--- 03/03.py
+++ 03/03.py
@@ -3,12 +3,7 @@ import numpy as np
 def valid_triangle(side_lengths):
     sides = list(side_lengths)
     longest = sides.pop(sides.index(max(side_lengths)))
-    remaining = 0
-    for side in sides:
-        remaining += side
-    if remaining > longest:
-        return True
-    return False
+    return sum(sides) > longest
 
 def count_valid(fname):
     valid = 0
@@ -22,6 +17,12 @@ def count_valid(fname):
                 invalid += 1
     return valid, invalid
 
+def reshape_array(ary):
+    newr = np.array(ary).T
+    new_tris = np.vstack([[col[i:i+3] for i in range(0,len(col),3)]
+                          for col in newr])
+    return new_tris
+    
 def count_valid2(fname):
     valid = 0
     invalid = 0
@@ -29,12 +30,7 @@ def count_valid2(fname):
     with open(fname) as thefile:
         for line in thefile:
             r.append([int(x) for x in line.split()])
-    newr = np.array(r).T
-    new_tris = []
-    for col in newr:
-        new_t = [col[i:i+3] for i in range(0, len(col), 3)]
-        for t in new_t:
-            new_tris.append(t)
+    new_tris = reshape_array(r)
     for triangle in new_tris:
         if valid_triangle(triangle):
             valid += 1

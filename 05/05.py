88737774f6ae38cc35c8668f73b0050606ca676c
ikehall <ike.hall@gmail.com>
2016-12-05 01:21:28 -0600

    make it prettier

--- 05/05.py
+++ 05/05.py
@@ -19,23 +19,25 @@ def char2_from_hash(hashstr):
     
 def hash_id_plus_index(strng, index):
     return hashlib.md5(strng+str(index)).hexdigest()
+
+def tprint(index, code):
+    if index % 1000000 == 0:
+        print code, index
     
-def find_password(doorid, start_index=0):
-    code = char_from_hash(hash_id_plus_index(doorid, start_index))
+def find_password(doorid, index=0):
+    code=''
     while len(code) < 8:
-        if start_index%1000000==0:
-            print code, start_index
-        start_index += 1
-        code += char_from_hash(hash_id_plus_index(doorid, start_index))
+        tprint(index, code)
+        code += char_from_hash(hash_id_plus_index(doorid, index))
+        index += 1
     return code
 
-def find_password2(doorid, start_index=0):
+def find_password2(doorid, index=0):
     code = ['-']*8
     while '-' in code:
-        if start_index%1000000==0:
-            print ''.join(code), start_index
-        pos, char = char2_from_hash(hash_id_plus_index(doorid, start_index))
-        start_index += 1
+        tprint(index, code)
+        pos, char = char2_from_hash(hash_id_plus_index(doorid, index))
+        index += 1
         if pos is not None and code[pos]=='-':
             code[pos] = char
     return ''.join(code)

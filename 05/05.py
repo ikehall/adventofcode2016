980939086d88a6fd531cd082ad08210733e250f0
ikehall <ike.hall@gmail.com>
2016-12-05 01:14:22 -0600

    make it faster

--- 05/05.py
+++ 05/05.py
@@ -2,26 +2,23 @@ import hashlib
 import itertools
 
 poss = '01234567'
+test_str = '00000'
 
 def char_from_hash(hashstr):
-    leading_digit, repeat = [(k, len(list(g)))
-                             for k,g in itertools.groupby(hashstr)][0]
-    if leading_digit != '0' or repeat < 5:
-        return ''
-    return hashstr[5]
-
+    if test_str == hashstr[:5]:
+        return hashstr[5]
+    return ''
+    
 def char2_from_hash(hashstr):
     if hashstr[5] not in poss:
         return None, None
-    leading_digit, repeat = [(k, len(list(g)))
-                             for k,g in itertools.groupby(hashstr)][0]
-    if leading_digit != '0' or repeat < 5:
+    if test_str == hashstr[:5]:
+        return int(hashstr[5]), hashstr[6]
+    else:
         return None, None
-    return int(hashstr[5]), hashstr[6]
     
 def hash_id_plus_index(strng, index):
     return hashlib.md5(strng+str(index)).hexdigest()
-
     
 def find_password(doorid, start_index=0):
     code = char_from_hash(hash_id_plus_index(doorid, start_index))
@@ -36,9 +33,9 @@ def find_password2(doorid, start_index=0):
     code = ['-']*8
     while '-' in code:
         if start_index%1000000==0:
-            print code, start_index
-        start_index += 1
+            print ''.join(code), start_index
         pos, char = char2_from_hash(hash_id_plus_index(doorid, start_index))
+        start_index += 1
         if pos is not None and code[pos]=='-':
             code[pos] = char
     return ''.join(code)
@@ -46,4 +43,5 @@ def find_password2(doorid, start_index=0):
 
 if __name__ == '__main__':
     import sys
+    print find_password(sys.argv[1])
     print find_password2(sys.argv[1])
